3e36d9514c6e7a9f2c1ea5d1117cfe83515a9b67
Ronald Gutierrez <ronaldgutierrezj@gmail.com>
2023-07-24 21:05:21 -0500

    bonus mission part 2

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // TODO: add code here
+window.addEventListener("load" , function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then( function(json){
+            // json = json.sort((a,b) => {
+            //     if(a.hoursInSpace > b.hoursInSpace){
+            //         return -1;
+            //     }
+            // })
+            const div = document.getElementById("container");
+            div.innerHTML = ``
+            for (let i = 0; i < json.length; i++){
+                div.innerHTML = div.innerHTML + `<div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName}  ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li class ="status">Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills}</li>
+                        </ul>
+                    </div>
+                    <img src=${json[i].picture} class="avatar">
+                </div>
+                `
+                
+                
+             
+            }
+            const status = document.getElementsByClassName("status");
+            console.log(status)
+            for(let i = 0 ; i < status.length; i++){
+                if(status[i].innerHTML == "Active: true"){
+                    status[i].style.color = "green"
+                }
+            }
+        })
+    })
+    
+})
